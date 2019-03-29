63d53d55d77379bab88a348c3c1b4f816b1951ca
TeodorPukhta <teodor.pukhta@gmail.com>
2019-03-29 09:34:21 +0200

    created api db ui

---
+++ pukhtaweb-api/src/main/java/pukhtaweb/api/PukhtawebApiApplication.java
@@ -0,0 +1,13 @@
+package pukhtaweb.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PukhtawebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PukhtawebApiApplication.class, args);
+	}
+
+}
