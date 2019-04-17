aa7bb7e1c50273920f0d63ec4ba644efbc796bb6
TeodorPukhta <teodor.pukhta@gmail.com>
2019-04-17 21:26:21 +0300

    working back

--- pukhtaweb-db/pukhtaweb-db.sql
+++ pukhtaweb-db/pukhtaweb-db.sql
@@ -2,9 +2,15 @@ CREATE TABLE "user"
 (
 	"id" SERIAL NOT NULL,
 	"email" VARCHAR(512) NOT NULL,
-	"passwordd" VARCHAR(50) NOT NULL,
-	"first_name" VARCHAR(255) NOT NULL,
-	"sur_name" VARCHAR(255) NOT NULL,
+	"phone" VARCHAR(14) NOT NULL,
+	"password" VARCHAR(50) NOT NULL,
+	"firstname" VARCHAR(255) NOT NULL,
+	"surname" VARCHAR(255) NOT NULL,
 	"is_active" BOOLEAN NOT NULL,
-	CONSTRAINT pk_user_id PRIMARY KEY("id")
+	CONSTRAINT pk_user_id PRIMARY KEY("id"),
+	CONSTRAINT uk_email UNIQUE ("email"),
+	CONSTRAINT uk_phone UNIQUE ("phone")
 );
+
+INSERT INTO "user" ("email","password","first_name","surname","phone","is_active")
+VALUES('user@gmail.com','123456','First','Sur','+380000000000','true');asd
